ffd3b09bf08c2476e5032ca428b039652e9d846e
亚博 张 <zyb15993675809@outlook.com>
2026-01-18 10:57:00 +0800

    save

--- 3_floatdouble.c
+++ 3_floatdouble.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+//#include <stdio.h>
 
 /*
 1、常用的类型：float（4字节）\ double（8字节）\ long double（12字节）
@@ -15,7 +15,7 @@
 		如果定义float类型的，则需要以“F”或者“f”作为后缀
 		如果定义long double类型的，则需要以“l”或者“L”作为后缀
 */
-
+/*
 int main()
 {
 	double a = 4.2;
@@ -26,16 +26,16 @@ int main()
 	float f2 = 5.2f;
 
 	long double d1 = 3.14L;
-
+*/
 /* 练习：有人用温度计测量出用华氏法表示的温度(如64°F），今要求把它转
 换为以摄氏法表示的温度(如17.8℃)。转换的公式为：c = 5(f-32)/9。
 其中，f 代表华氏温度，c 代表摄氏温度。
 */
-
+/*
 	double f, c;
 	f = 64.0;
 	c = 5.0 * (f - 32) / 9;
 	printf("c=%.2f\nf=%.2f", c, f);
 
 	return 0;
-}
+}*/
