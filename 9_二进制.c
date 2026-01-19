16f29344a46cd6ae00edf3686239c0bd455a218c
亚博 张 <zyb15993675809@outlook.com>
2026-01-19 21:23:02 +0800

    save

---
+++ 9_二进制.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+	int n1 = 10;		//十进制
+	int n2 = 0b10;		//二进制	
+	int n3 = 017;		//八进制
+	int n4 = 0x1a;		//十六进制
+
+	//以十进制的方式打印
+	printf("%d\n", n1);
+	printf("%d\n", n2);
+	printf("%d\n", n3);
+	printf("%d\n", n4);
+
+	//以不同进制的方式打印
+	int x = 100;
+	printf("dec = %d\n", x); // 100		十进制整数。
+	printf("octal = %o\n", x); // 144	八进制整数。
+	printf("hex = %x\n", x); // 64		十六进制整数。
+	printf("octal = %#o\n", x); // 0144	显示前缀 0 的八进制整数。
+	printf("hex = %#x\n", x); // 0x64	显示前缀 0x 的十六进制整数。
+	printf("hex = %#X\n", x); // 0X64	显示前缀 0X 的十六进制整数。
+
+	return 0;
+}
